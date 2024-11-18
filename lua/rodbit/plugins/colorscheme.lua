f00d841709e94c8e93fa301457a8e2e53c020512
rodbit-studios <rodbit.studios@gmail.com>
2024-11-18 13:46:10 -0500

    added colorizer and fixed colorscheme

--- lua/rodbit/plugins/colorscheme.lua
+++ lua/rodbit/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-	{
-		"rodbit-studios/rodbit-theme.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme rodbit-theme")
-		end,
-	},
+	"rodbit-studios/rodbit-theme.nvim",
+	priority = 1000, -- ensure it loads very early
+	lazy = false, -- load immediately
+	config = function()
+		-- Use pcall to prevent errors from stopping startup
+		local ok, _ = pcall(function()
+			vim.cmd("colorscheme rodbit-theme") -- or your theme name
+		end)
+		if not ok then
+			vim.notify("Error loading colorscheme", vim.log.levels.ERROR)
+		end
+	end,
 }
