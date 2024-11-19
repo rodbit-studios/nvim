663eac1e62f14bc39cf1c8ffac09041693b79662
rodbit-studios <rodbit.studios@gmail.com>
2024-11-19 13:15:28 -0500

    Back to Tokyonight colorscheme

--- lua/rodbit/plugins/colorscheme.lua
+++ lua/rodbit/plugins/colorscheme.lua
@@ -1,14 +1,9 @@
 return {
-	"rodbit-studios/rodbit-theme.nvim",
-	priority = 1000, -- ensure it loads very early
-	lazy = false, -- load immediately
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
 	config = function()
-		-- Use pcall to prevent errors from stopping startup
-		local ok, _ = pcall(function()
-			vim.cmd("colorscheme rodbit-theme") -- or your theme name
-		end)
-		if not ok then
-			vim.notify("Error loading colorscheme", vim.log.levels.ERROR)
-		end
+		vim.cmd([[colorscheme tokyonight-moon]])
 	end,
 }
